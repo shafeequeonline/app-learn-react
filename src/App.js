6181bc189ea78d79630849bd72c7d402f2359c1f
Mohamed Shafeeque <mshafeequemsn@gmail.com>
2021-02-22 09:47:43 +0530

    React: Added first functional component with prop and children

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ class App extends Component {
             <div className="App">
                 <h1>Hi, I am react App</h1>
                 <p>This is really working!</p>
-                <Person />
-                <Person />
-                <Person />
+                <Person name="Shafeeque" age="31" />
+                <Person name="Rajeev" age="30" />
+                <Person name="Umer" age="29">
+                    I love Tea!
+                </Person>
             </div>
         );
     }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
-const person = () => {
+const person = (props) => {
     // Add singlue curly braces to use JS expressions
-    return <p>I am a person and I am {Math.floor(Math.random() * 30)} years old.</p>
+    // return <p>I am  and I am {Math.floor(Math.random() * 30)} years old.</p>
+    return (
+        <div>
+            <p>I am {props.name}, {props.age} years old.</p>
+            <p>{props.children}</p>
+        </div>
+    )
 }
 
 export default person;
