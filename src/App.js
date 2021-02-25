8ade7b6e5e51af6da68754b89d35420fd3534dd6
Mohamed Shafeeque <mshafeequemsn@gmail.com>
2021-02-25 08:54:35 +0530

    React: bind method and using fuctions instead of bind

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ class App extends Component {
         ]
     }
 
-    switchNameHandler = () => {
+    switchNameHandler = (newName) => {
         this.setState( {
             persons : [
-                { name: 'Mohamed Shafeeque', age: 31 },
+                { name: newName, age: 31 },
                 { name: 'Rajeev K Ravi', age: 30 },
                 { name: 'Umer Farook', age: 29 }
             ]
@@ -28,8 +28,8 @@ class App extends Component {
             <div className="App">
                 <h1>Hi, I am react App</h1>
                 <p>This is really working!</p>
-                <button onClick={this.switchNameHandler}>Switch Names</button>
-                <Person click={this.switchNameHandler} name={this.state.persons[0].name} age={this.state.persons[0].age} />
+                <button onClick={() => this.switchNameHandler('M.Shafeeque!')}>Switch Names</button>
+                <Person click={this.switchNameHandler.bind(this, 'MS Naripatta')} name={this.state.persons[0].name} age={this.state.persons[0].age} />
                 <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
                 <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
                     I love Tea and Movies!
