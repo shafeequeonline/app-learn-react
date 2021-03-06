d6d93a9b33fee835a39c1b6b053e691e1ff700d9
Mohamed Shafeeque <mshafeequemsn@gmail.com>
2021-03-06 19:50:37 +0530

    React: Adding CSS Style

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './Person.css'
 
 const person = (props) => {
     // Add singlue curly braces to use JS expressions
     // return <p>I am  and I am {Math.floor(Math.random() * 30)} years old.</p>
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I am {props.name}, {props.age} years old.</p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.click} />
+            <input type="text" onChange={props.click} value={props.name} />
         </div>
     )
 }
