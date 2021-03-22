aadac332f2d2a5a71fdf8f950c98e488d46fa00b
Mohamed Shafeeque <mshafeequemsn@gmail.com>
2021-03-22 21:59:17 +0530

    two way binding in React

--- src/BindingDemo.js
+++ src/BindingDemo.js
@@ -2,12 +2,17 @@ import React, { useState } from 'react'
 
 const bindingExample = () => {
     const [labelText, setLabelText] = useState("MY NEW STRING");
+    const [place, setPlace] = useState('Malappuram')
 
     return (
         <div>
-            <lable>{labelText}</lable> <br/>
-            <input type="text" name="labelTextField" value={this.labelText} onChange={(e) => {setLabelText(e.target.value)}}></input>
+            <label>{labelText}</label> <br/>
+            <input type="text" name="labelTextField" value={labelText} onChange={(e) => {setLabelText(e.target.value)}}></input>
             <br/><br/>
+
+
+            <label>{place}</label><br/>
+            <input type="text" value={place} onChange={(e) => {setPlace(e.target.value)}} />
         </div>
     )
 }
