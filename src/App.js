c581ec378371c36eecbd5b822b934904595cefb5
Mohamed Shafeeque <mshafeequemsn@gmail.com>
2021-04-14 10:56:45 +0530

    conditional checking advanced method

--- src/App.js
+++ src/App.js
@@ -109,42 +109,4 @@ class App extends Component {
     }
 }
 
-
 export default App;
-
-
-// const app = (props) => {
-//     const [personState, setPersonState] = useState({
-//         persons : [
-//             { name: 'Shafeeque', age: 31 },
-//             { name: 'Rajeev', age: 30 },
-//             { name: 'Umer', age: 29 }
-//         ]
-//     })
-
-//     const switchNameHandler = () => {
-//         setPersonState( {
-//             persons : [
-//                 { name: 'Mohamed Shafeeque', age: 31 },
-//                 { name: 'Rajeev K Ravi', age: 30 },
-//                 { name: 'Umer Farook', age: 29 }
-//             ]
-//         })
-//     }
-    
-
-//     return (
-//         <div className="App">
-//             <h1>Hi, I am react App</h1>
-//             <p>This is really working!</p>
-//             <button onClick={switchNameHandler}>Switch Names</button>
-//             <Person name={personState.persons[0].name} age={personState.persons[0].age} />
-//             <Person name={personState.persons[1].name} age={personState.persons[1].age} />
-//             <Person name={personState.persons[2].name} age={personState.persons[2].age}>
-//                 I love Tea!
-//             </Person>
-//         </div>
-//     );
-// }
-
-// export default app;
